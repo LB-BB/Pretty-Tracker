ccf12c8e7b2ef58b7955dd4c486e7e765d219fa3
LB-BB <57300241+LB-BB@users.noreply.github.com>
2024-01-22 12:51:48 -0800

    style active link in navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,9 +1,12 @@
+'use client'
 import Link from 'next/link'
 import React from 'react'
 import { GiFlyingBeetle } from "react-icons/gi"
-
+import { usePathname } from 'next/navigation'
+import classnames from 'classnames'
 
 const NavBar = () => {
+  const currentPath = usePathname()
   const links = [
     {label: 'Dashboard', href:'/'},
     {label: 'Issues', href:'/issues'}
@@ -15,10 +18,12 @@ const NavBar = () => {
         {links.map(link => 
           <Link 
             key={link.href} 
-            href={link.href} 
-            className='text-zinc-500 hover:text-zinc-800 transition-colors'>
-              {link.label}
-          </Link>)}
+            className={classnames({
+              'text-zinc-500':  link.href === currentPath,
+              'text-zinc-900': link.href !== currentPath,
+              'hover:text-zinc-800 transition-colors': true
+            })}
+            href={link.href}>{link.label}</Link>)}
       </ul>
     </nav>
   )
